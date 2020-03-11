e832255290268263ae4e8b123dc05ff0d43032e4
Gunnala <madhavigunnala@gmail.com>
2020-03-11 11:26:29 -0400

    commiting Adding

---
+++ eclipse-workspace/Practice sessions/src/Adding.java
@@ -0,0 +1,37 @@
+
+public class Adding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//webdriver driver = new webdriver();
+		
+			
+		//printing nos from  0 ---10
+		
+		int i  = 1;    ///initilization
+		while (i<=10){      //condition
+			System.out.println(i);
+	        i = i +1;       //incremental  or deremental
+		}	
+		
+
+		System.out.println("*******");
+		
+		 //printing nos from 10...0
+		int j = 10;
+		while (j >=0 ) {
+			System.out.println(j);
+			j = j - 1;
+		}
+
+		System.out.println("--------");
+		for (int x =1; x<=10; x= x+1) {
+		System.out.println(x);
+			
+	}
+		
+	}
+
+}
